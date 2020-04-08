5526aee16509c5e1dc94a62787f2850bb36994b7
Arul Kolla <arulkolla@gmail.com>
2020-04-08 11:29:00 -0700

    Add solution for problem `A - Petya and Strings` Link: https://codeforces.com/contest/112/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/112/submission/75918935

---
+++ codeforces/112/A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ctype.h>
+#include <algorithm>
+ 
+using namespace std;
+ 
+int main() {
+    std::string p, q;
+    cin >> p >> q;
+    int i = 0;
+    for (i = 0; i < p.size(); i++) {
+        if ((int)tolower(p[i]) > (int)tolower(q[i])) {cout << 1; return 0;}
+        else if ((int)tolower(p[i]) < (int)tolower(q[i])) {cout << -1; return 0;}
+    }
+    cout << 0;
+    return 0;
+}
