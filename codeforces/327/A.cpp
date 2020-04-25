1ef7f15e296438a3fe650d5ecc0b56ed5612c85b
Arul Kolla <arulkolla@gmail.com>
2020-04-24 18:00:00 -0700

    Add solution for problem `A - Flipping Game` Link: https://codeforces.com/contest/327/problem/A Tags: brute force, dp, implementation, *1200 Ref: https://codeforces.com/contest/327/submission/77966309

---
+++ codeforces/327/A.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int n, ans = 0;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            int cnt = 0;
+            for (int k = 0; k < i; k++) {if (a[k] == 1) {cnt++;}}
+            for (int k = j + 1; k < n; k++) {if (a[k]==1) {cnt++;}}
+            for (int k = i; k <= j; k++) {if (a[k]==0) {cnt++;}}
+            ans = max(ans,cnt);
+        }
+    }
+    cout << ans;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+//    int tt;
+//    cin >> tt;
+//    for (int i = 1; i <= tt; i++) {
+//        solve();
+//        cout << endl;
+//    }
+    solve();
+}
