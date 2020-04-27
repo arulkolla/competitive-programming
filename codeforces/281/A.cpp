71d89a1bdbe7a151337d90a7cef39c47f5408a1e
Arul Kolla <arulkolla@gmail.com>
2020-04-27 12:16:00 -0700

    Add solution for problem `A - Word Capitalization` Link: https://codeforces.com/contest/281/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/281/submission/78324273

---
+++ codeforces/281/A.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    string s;
+    cin >> s;
+    s[0] = toupper(s[0]);
+    cout << s;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+//    int tt;
+//    cin >> tt;
+//    for (int i = 1; i <= tt; i++) {
+//        solve();
+//        cout << endl;
+//    }
+    solve();
+}
