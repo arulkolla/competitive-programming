52c25862872671360a6ae29304fb49aa10da2cee
Arul Kolla <arulkolla@gmail.com>
2020-04-27 11:59:00 -0700

    Add solution for problem `A - Again Twenty Five!` Link: https://codeforces.com/contest/630/problem/A Tags: number theory, *800 Ref: https://codeforces.com/contest/630/submission/78323195

---
+++ codeforces/630/A.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << 25;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+//    int tt;
+//    cin >> tt;
+//    for (int i = 1; i <= tt; i++) {
+//        solve();
+//        cout << endl;
+//    }
+    solve();
+}
