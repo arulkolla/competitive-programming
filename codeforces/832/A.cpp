561c2df36591a2d82c585203be8cff55c1b12e79
Arul Kolla <arulkolla@gmail.com>
2020-04-27 12:13:00 -0700

    Add solution for problem `A - Sasha and Sticks` Link: https://codeforces.com/contest/832/problem/A Tags: games, math, *800 Ref: https://codeforces.com/contest/832/submission/78324078

---
+++ codeforces/832/A.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    long long n, k;
+    cin >> n >> k;
+    if (((n - (n % k)) / k) % 2 == 1) {cout << "YES";}
+    else {cout << "NO";}
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+//    int tt;
+//    cin >> tt;
+//    for (int i = 1; i <= tt; i++) {
+//        solve();
+//        cout << endl;
+//    }
+    solve();
+}
