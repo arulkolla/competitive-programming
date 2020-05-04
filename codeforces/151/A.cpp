0efe6d5375cdc8823504b0db81a41b76abc4b7a4
Arul Kolla <arulkolla@gmail.com>
2020-05-04 16:16:00 -0700

    Add solution for problem `A - Soft Drinking` Link: https://codeforces.com/contest/151/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/151/submission/79009733

---
+++ codeforces/151/A.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int n, k, l, c, d, p, nl, np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+    cout << min(k*l/nl, min(c*d, p/np))/n;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+//    int tt;
+//    cin >> tt;
+//    for (int i = 1; i <= tt; i++) {
+//        solve();
+//        cout << endl;
+//    }
+    solve();
+}
