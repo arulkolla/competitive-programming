7214878c4106f8934c9083b69c0d230c3fdb56ce
Arul Kolla <arulkolla@gmail.com>
2020-05-04 16:25:00 -0700

    Add solution for problem `A - I'm bored with life` Link: https://codeforces.com/contest/822/problem/A Tags: implementation, math, number theory, *800 Ref: https://codeforces.com/contest/822/submission/79009982

---
+++ codeforces/822/A.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+long long factorial (int x) {
+    if (x == 1) {return 1;}
+    return x*factorial(x-1);
+}
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    cout << factorial(min(a,b));
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+//    int tt;
+//    cin >> tt;
+//    for (int i = 1; i <= tt; i++) {
+//        solve();
+//        cout << endl;
+//    }
+    solve();
+}
