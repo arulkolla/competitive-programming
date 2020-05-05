91edfbfbc50513165e685e1f75defe838aaa754e
Arul Kolla <arulkolla@gmail.com>
2020-05-04 22:24:00 -0700

    Add solution for problem `A - Hit the Lottery` Link: https://codeforces.com/contest/996/problem/A Tags: dp, greedy, *800 Ref: https://codeforces.com/contest/996/submission/79023255

---
+++ codeforces/996/A.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    int res = 0;
+    for (int i : {100, 20, 10, 5, 1}) {
+        res += n/i;
+        n %= i;
+    }
+    cout << res;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+//    int tt;
+//    cin >> tt;
+//    for (int i = 1; i <= tt; i++) {
+//        solve();
+//        cout << endl;
+//    }
+    solve();
+}
