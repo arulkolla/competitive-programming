84b9b5ee2e82fa63c5a19af702a06a138588c655
Arul Kolla <arulkolla@gmail.com>
2020-05-06 07:40:00 -0700

    Add solution for problem `A - Puzzle Pieces` Link: https://codeforces.com/contest/1345/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1345/submission/79150489

---
+++ codeforces/1345/A.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int m, n;
+    cin >> m >> n;
+    if (m == 1 || n == 1 || ((n == 2) && (m == 2))) {cout << "YES";}
+    else {cout << "NO";}
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt;
+    cin >> tt;
+    for (int i = 1; i <= tt; i++) {
+        solve();
+        cout << endl;
+    }
+//    solve();
+}
