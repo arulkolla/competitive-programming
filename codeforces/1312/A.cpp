2eb6182dc7e6f668a47f479177bc714e6d69bf7a
Arul Kolla <arulkolla@gmail.com>
2020-05-23 09:24:00 -0700

    Add solution for problem `A - Two Regular Polygons` Link: https://codeforces.com/contest/1312/problem/A Tags: geometry, greedy, math, number theory, *800 Ref: https://codeforces.com/contest/1312/submission/81031092

---
+++ codeforces/1312/A.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int m, n;
+    cin >> m >> n;
+    if (m % n == 0) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve(); cout << endl;}
+//    solve();
+}
