a2c5e9d9a2682c1e4801acd25d2a3f0e4e3f0932
Arul Kolla <arulkolla@gmail.com>
2020-05-25 21:36:00 -0700

    Add solution for problem `B - Buttons` Link: https://codeforces.com/contest/268/problem/B Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/268/submission/81435558

---
+++ codeforces/268/B.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    unsigned long long int n;
+    cin >> n;
+    cout << (n * (n*n + 5))/6;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+//    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve(); cout << endl;}
+    solve();
+}
