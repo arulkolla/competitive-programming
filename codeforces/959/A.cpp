5f94752e763358e61ba9f3390c6a8791675fdca9
Arul Kolla <arulkolla@gmail.com>
2020-05-25 21:21:00 -0700

    Add solution for problem `A - Mahmoud and Ehab and the even-odd game` Link: https://codeforces.com/contest/959/problem/A Tags: games, math, *800 Ref: https://codeforces.com/contest/959/submission/81434913

---
+++ codeforces/959/A.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << (n % 2 == 0 ? "Mahmoud" : "Ehab");
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+//    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve(); cout << endl;}
+    solve();
+}
