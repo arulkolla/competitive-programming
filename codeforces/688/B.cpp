85a005de16932550a1103e4fd14552653cec3701
Arul Kolla <arulkolla@gmail.com>
2020-05-25 18:02:00 -0700

    Add solution for problem `B - Lovely Palindromes` Link: https://codeforces.com/contest/688/problem/B Tags: constructive algorithms, math, *1000 Ref: https://codeforces.com/contest/688/submission/81427827

---
+++ codeforces/688/B.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    string s;
+    cin >> s;
+    cout << s;
+    for (int i = 0; i < s.size(); i++) {
+        cout << s[s.size() - i - 1];
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+//    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve(); cout << endl;}
+    solve();
+}
