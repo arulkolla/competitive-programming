348eefd2e97f1ae999b7ccddee5570a8b6f454d4
Arul Kolla <arulkolla@gmail.com>
2020-06-01 14:40:00 -0700

    Add solution for problem `B - ... (Triple Dots)` Link: https://atcoder.jp/contests/abc168/tasks/abc168_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc168/submissions/13938177

---
+++ atcoder/abc168/B.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+void solve() {
+    int k;
+    string s;
+    cin >> k;
+    cin >> s;
+    if (s.length() <= k) {cout << s;}
+    else {
+        for (int i = 0; i < k; i++) {
+            cout << s[i];
+        }
+        cout << "...";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve(); cout << endl;}
+    solve();
+}
