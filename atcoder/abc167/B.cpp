6d327237cf43c310529c9d59f489d103ba5661c8
Arul Kolla <arulkolla@gmail.com>
2020-06-23 22:51:00 -0700

    Add solution for problem `B - Easy Linear Programming` Link: https://atcoder.jp/contests/abc167/tasks/abc167_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc167/submissions/14650466

---
+++ atcoder/abc167/B.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+    int a, b, c, k;
+    cin >> a >> b >> c >> k;
+    int res = 0;
+    if (k <= a) {cout << k;}
+    else if (k <= a + b) {cout << a;}
+    else {
+        cout << a - (k - a - b);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve(); cout << endl;}
+    solve();
+}
