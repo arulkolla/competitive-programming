c0110b1564a4d47d78805de4364fcccced569a7d
Arul Kolla <arulkolla@gmail.com>
2020-06-23 22:45:00 -0700

    Add solution for problem `A - Registration` Link: https://atcoder.jp/contests/abc167/tasks/abc167_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc167/submissions/14650365

---
+++ atcoder/abc167/A.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+    string s, t;
+    cin >> s;
+    cin >> t;
+    bool ok = true;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] != t[i]) {
+            ok = false;
+        }
+    }
+    cout << (ok ? "Yes" : "No");
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve(); cout << endl;}
+    solve();
+}
