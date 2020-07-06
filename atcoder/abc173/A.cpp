7371205b7fc3aedc15032abb01bd6d24a5240aea
Arul Kolla <arulkolla@gmail.com>
2020-07-06 16:36:00 -0700

    Add solution for problem `A - Payment` Link: https://atcoder.jp/contests/abc173/tasks/abc173_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc173/submissions/15056333

---
+++ atcoder/abc173/A.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+const int MAX = 507;
+const int MOD = 1000000007;
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << (1000 - n % 1000) % 1000;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve(); cout << endl;}
+    solve();
+}
