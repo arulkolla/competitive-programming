4180c5391a65424296ea793639fa65c7ad062127
Arul Kolla <arulkolla@gmail.com>
2020-08-27 21:50:00 -0700

    Add solution for problem `B - Echo` Link: https://atcoder.jp/contests/abc145/tasks/abc145_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc145/submissions/16274836

---
+++ atcoder/abc145/B.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+const int MAX = 107;
+const int MOD = 1000000007;
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    if (n % 2) {cout << "No"; return;}
+    for (int i = 0; i < n / 2; i++) {
+        if (s[i] != s[i + n / 2]) {cout << "No"; return;}
+    }
+    cout << "Yes";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //freopen("in.in", "r", stdin); freopen("out.out", "w", stdout);
+    //int tt; cin >> tt; for (int i = 1; i <= tt; i++) {cout << "Case #" << i << ": "; solve();}
+    solve();
+}
