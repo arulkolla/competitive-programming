5c74756c1d687c37b27adf35cf7e81a3b2df758d
Arul Kolla <arulkolla@gmail.com>
2020-09-16 11:06:00 -0700

    Add solution for problem `A - Plate Game` Link: https://codeforces.com/contest/197/problem/A Tags: constructive algorithms, games, math, *1600 Ref: https://codeforces.com/contest/197/submission/92999155

---
+++ codeforces/197/A.cpp
@@ -0,0 +1,47 @@
+
+// Problem : A. Plate Game
+// Contest : Codeforces - Codeforces Round #124 (Div. 2)
+// URL : https://codeforces.com/problemset/problem/197/A
+// Memory Limit : 256 MB
+// Time Limit : 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+	int a, b, r;
+	cin >> a >> b >> r;
+	cout << ((2 * r > a || 2 * r > b) ? "Second" : "First");
+	// if (2 * r >= a || 2 * r >= b) {}
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //freopen("in.in", "r", stdin); freopen("out.out", "w", stdout);
+    //int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+    solve();
+}
