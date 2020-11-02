805cc32b869ab61cf5fe0dafea340c223d74f644
Arul Kolla <arulkolla@gmail.com>
2020-11-02 09:41:00 -0800

    Add solution for problem `A - Kids Seating` Link: https://codeforces.com/contest/1443/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1443/submission/97497947

---
+++ codeforces/1443/A.cpp
@@ -0,0 +1,31 @@
+
+// Problem: A. Kids Seating
+// Contest: Codeforces - Codeforces Round #681 (Div. 2, based on VK Cup 2019-2020 - Final)
+// URL: https://codeforces.com/contest/1443/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+	int n;
+	cin >> n;
+	for (int i = n; i < 2 * n; i++) {
+		cout << 2 * i << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //freopen("in.in", "r", stdin); freopen("out.out", "w", stdout);
+    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+    //solve();
+}
