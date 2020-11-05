c6f79a4d4d90a36792ea76814a3934445dcf58c7
Arul Kolla <arulkolla@gmail.com>
2020-11-04 21:46:00 -0800

    Add solution for problem `B - Party` Link: https://codeforces.com/contest/23/problem/B Tags: constructive algorithms, graphs, math, *1600 Ref: https://codeforces.com/contest/23/submission/97677072

---
+++ codeforces/23/B.cpp
@@ -0,0 +1,28 @@
+
+// Problem: B. Party
+// Contest: Codeforces - Codeforces Beta Round #23
+// URL: https://codeforces.com/problemset/problem/23/B
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+	int n;
+	cin >> n;
+	cout << max(0, n - 2) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //freopen("in.in", "r", stdin); freopen("out.out", "w", stdout);
+    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+    //solve();
+}
