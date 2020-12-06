099eec02396348b0418b5d1166068ceec8c37f04
Arul Kolla <arulkolla@gmail.com>
2020-12-06 06:36:00 -0800

    Add solution for problem `A - Avoid Trygub` Link: https://codeforces.com/contest/1450/problem/A Tags: constructive algorithms, sortings, *800 Ref: https://codeforces.com/contest/1450/submission/100524713

---
+++ codeforces/1450/A.cpp
@@ -0,0 +1,31 @@
+
+// Problem: A. Avoid Trygub
+// Contest: Codeforces - Codeforces Global Round 12
+// URL: https://codeforces.com/contest/1450/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	sort(s.begin(), s.end());
+	cout << s << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //freopen("in.in", "r", stdin); freopen("out.out", "w", stdout);
+    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+    //solve();
+}
