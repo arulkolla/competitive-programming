a857884eda1c6377fa63f83ca3656f880a29002e
Arul Kolla <arulkolla@gmail.com>
2020-12-08 08:22:00 -0800

    Add solution for problem `D - Ticket Game` Link: https://codeforces.com/contest/1215/problem/D Tags: games, greedy, math, *1700 Ref: https://codeforces.com/contest/1215/submission/100712811

---
+++ codeforces/1215/D.cpp
@@ -0,0 +1,39 @@
+
+// Problem: D. Ticket Game
+// Contest: Codeforces - Codeforces Round #585 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/1215/D
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	long long q = 0, v = 0;
+	for (int i = 0; i < n / 2; i++) {
+		if (s[i] == '?') {q++;}
+		else {v += (s[i] - '0');}
+	}
+	for (int i = n / 2; i < n; i++) {
+		if (s[i] == '?') {q--;}
+		else {v -= (s[i] - '0');}
+	}
+	cout << ((2 * v + 9 * q != 0) ? "Monocarp" : "Bicarp");
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //freopen("in.in", "r", stdin); freopen("out.out", "w", stdout);
+    //int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+    solve();
+}
