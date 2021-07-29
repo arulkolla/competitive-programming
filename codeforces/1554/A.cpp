66d93811136c9c16d453bb4cbb95061295729a3c
Arul Kolla <arulkolla@gmail.com>
2021-07-29 07:38:00 -0700

    Add solution for problem `A - Cherry` Link: https://codeforces.com/contest/1554/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1554/submission/124129740

---
+++ codeforces/1554/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+	int n;
+	cin >> n;
+	long long a[n + 7];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	long long best = -1;
+	for (int i = 0; i < n - 1; i++) {
+		best = max(best, a[i] * a[i + 1]);
+	}
+	cout << best << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+    // solve();
+}
