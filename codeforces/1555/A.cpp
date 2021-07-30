d85a8618c3bf7f9a1a8096d22b17c66c8fd53f1e
Arul Kolla <arulkolla@gmail.com>
2021-07-30 07:37:00 -0700

    Add solution for problem `A - PizzaForces` Link: https://codeforces.com/contest/1555/problem/A Tags: brute force, math, *900 Ref: https://codeforces.com/contest/1555/submission/124279756

---
+++ codeforces/1555/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+	long long n;
+	cin >> n;
+	if (n <= 6) {n = 6;}
+	else if (n % 2 == 1) {n++;}
+	cout << (n * 5ll) / 2 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+    // solve();
+}
