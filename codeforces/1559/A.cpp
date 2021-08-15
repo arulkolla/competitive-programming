26c52249b8092202d955e8818c2188fd4ab151d5
Arul Kolla <arulkolla@gmail.com>
2021-08-15 07:38:00 -0700

    Add solution for problem `A - Mocha and Math` Link: https://codeforces.com/contest/1559/problem/A Tags: bitmasks, constructive algorithms, math, *900 Ref: https://codeforces.com/contest/1559/submission/125936187

---
+++ codeforces/1559/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+	int n;
+	cin >> n;
+	int res, a;
+	cin >> res;
+	for (int i = 1; i < n; i++) {
+		cin >> a;
+		res &= a;
+	}
+	cout << res << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+    // solve();
+}
