bbccf88b0248131c047f65ac7a5ca0e4bd681329
Arul Kolla <arulkolla@gmail.com>
2022-04-01 08:03:00 -0700

    Add solution for problem `A - Who Tested?` Link: https://codeforces.com/contest/1663/problem/A Tags: *special problem, expression parsing, trees Ref: https://codeforces.com/contest/1663/submission/152343969

---
+++ codeforces/1663/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+	cout << "BucketPotato";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+    solve();
+}
