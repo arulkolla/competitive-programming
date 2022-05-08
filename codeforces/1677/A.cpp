498efebc64794be1b6b3599484d2f4f306a59ceb
Arul Kolla <arulkolla@gmail.com>
2022-05-08 11:39:00 -0700

    Add solution for problem `A - Tokitsukaze and Strange Inequality` Link: https://codeforces.com/contest/1677/problem/A Tags: brute force, data structures, dp, *1600 Ref: https://codeforces.com/contest/1677/submission/156357802

--- codeforces/1677/A.cpp
+++ codeforces/1677/A.cpp
@@ -2,10 +2,8 @@
 
 using namespace std;
 
-const int MAX = 5007;
+const int MAX = 200007;
 const int MOD = 1000000007;
-
-long long prefl[MAX][MAX] = {};
 
 void solve() {
 	int n;
@@ -14,15 +12,24 @@ void solve() {
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
 	}
+	long long prefl[n + 7][n + 7];
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= n; j++) {
+			prefl[i][j] = 0LL;
+		}
+	}
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
 			prefl[i][j] = prefl[i][j - 1] + prefl[i - 1][j] - prefl[i - 1][j - 1] + (a[i] < a[j]);
 		}
 	}
 	long long res = 0;
-	for (int i = 2; i <= n; i++) {
+	for (int i = 1; i <= n; i++) {
 		for (int j = i + 2; j <= n; j++) {
-		    res += (prefl[i - 1][j - 1] - prefl[0][j - 1] - prefl[i - 1][i] + prefl[0][i]) * (a[i] > a[j]);
+			if (a[i] > a[j]) {
+				if (i < 2 || i + 2 > j) {continue;}
+				res += prefl[i - 1][j - 1] - prefl[0][j - 1] - prefl[i - 1][i] + prefl[0][i];
+			}
 		}
 	}
 	cout << res << '\n';
