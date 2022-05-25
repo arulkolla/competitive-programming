a89ee3f238391a8686ef0a7d942b50cc05342128
Arul Kolla <arulkolla@gmail.com>
2022-05-25 10:40:00 -0700

    Add solution for problem `B - Odd Subarrays` Link: https://codeforces.com/contest/1686/problem/B Tags: dp, greedy, *800 Ref: https://codeforces.com/contest/1686/submission/158416346

---
+++ codeforces/1686/B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+	int n;
+	cin >> n;
+	int a[n], res = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	for (int i = 0; i < n - 1; i++) {
+		if (a[i] > a[i + 1]) {
+			res++;
+			i++;
+		}
+	}
+	cout << res << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+    // solve();
+}
