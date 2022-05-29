ff67d2db44844d4193846094a06665f2a423833d
Arul Kolla <arulkolla@gmail.com>
2022-05-29 10:26:00 -0700

    Add solution for problem `A - Display The Number` Link: https://codeforces.com/contest/1295/problem/A Tags: greedy, *900 Ref: https://codeforces.com/contest/1295/submission/158820696

---
+++ codeforces/1295/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+	int n;
+	cin >> n;
+	cout << (n % 2 == 0 ? 1 : 7);
+	for (int i = 0; i < n - 3; i += 2) {cout << 1;}
+	cout << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+    // solve();
+}
