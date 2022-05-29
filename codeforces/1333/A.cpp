2ac7b1059488f956334d1e5ce04823e8072beccd
Arul Kolla <arulkolla@gmail.com>
2022-05-29 10:28:00 -0700

    Add solution for problem `A - Little Artem` Link: https://codeforces.com/contest/1333/problem/A Tags: constructive algorithms, *1000 Ref: https://codeforces.com/contest/1333/submission/158820854

---
+++ codeforces/1333/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (i == 0 && j == 0) {cout << 'W';}
+			else {cout << 'B';}
+		}
+		cout << '\n';
+	}
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+    // solve();
+}
